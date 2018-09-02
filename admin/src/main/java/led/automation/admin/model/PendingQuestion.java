4c37846769b1c2e11322ff1677374fa3a1ffe068
gederanadewadatta <gede.dewadatta@ledconsulting.co.id>
2018-09-02 17:22:37 +0700

    add class for handling pending question

---
+++ admin/src/main/java/led/automation/admin/model/PendingQuestion.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package led.automation.admin.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+
+/**
+ * @author gederanadewadatta
+ *
+ */
+public class PendingQuestion {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SUBMITQUESTION_SEQ")
+	@SequenceGenerator(sequenceName = "submitquestion_seq", allocationSize = 1, name = "SUBMITQUESTION_SEQ")
+	private Long id;
+	@Column(name = "EMPLOYEE_CODE")
+	private String employeeCode;
+	@Column(name="ID_QUESTION")
+	private Long idQuestion; 
+}
