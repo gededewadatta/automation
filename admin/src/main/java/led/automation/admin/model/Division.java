bfd64d25753d9ef883ac559b2c5b8ee6450ad3dd
gederanadewadatta <gede.dewadatta@ledconsulting.co.id>
2018-09-02 22:07:41 +0700

    revision duplicate column
    Signed-off-by: gederanadewadatta <gede.dewadatta@ledconsulting.co.id>

--- admin/src/main/java/led/automation/admin/model/Division.java
+++ admin/src/main/java/led/automation/admin/model/Division.java
@@ -29,6 +29,86 @@ public class Division {
     
     @Column(name = "CREATED_BY")
     private String createdBy;
+    
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Division [id=" + id + ", divisonCode=" + divisonCode + ", divisionName=" + divisionName
+				+ ", createdDate=" + createdDate + ", createdBy=" + createdBy + "]";
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the divisonCode
+	 */
+	public String getDivisonCode() {
+		return divisonCode;
+	}
+
+	/**
+	 * @param divisonCode the divisonCode to set
+	 */
+	public void setDivisonCode(String divisonCode) {
+		this.divisonCode = divisonCode;
+	}
+
+	/**
+	 * @return the divisionName
+	 */
+	public String getDivisionName() {
+		return divisionName;
+	}
+
+	/**
+	 * @param divisionName the divisionName to set
+	 */
+	public void setDivisionName(String divisionName) {
+		this.divisionName = divisionName;
+	}
+
+	/**
+	 * @return the createdDate
+	 */
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	/**
+	 * @param createdDate the createdDate to set
+	 */
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	/**
+	 * @return the createdBy
+	 */
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	/**
+	 * @param createdBy the createdBy to set
+	 */
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
  
     
     
