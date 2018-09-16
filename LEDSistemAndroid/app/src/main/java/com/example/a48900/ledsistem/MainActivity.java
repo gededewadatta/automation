eb4ed6fb143946d3eda7c894d2ad04d54c4144d7
muhammadBurhanuddin <gede.dewadatta@ledconsulting.co.id>
2018-09-16 20:51:07 +0700

    Test_Claudia

--- LEDSistemAndroid/app/src/main/java/com/example/a48900/ledsistem/MainActivity.java
+++ LEDSistemAndroid/app/src/main/java/com/example/a48900/ledsistem/MainActivity.java
@@ -1,53 +1,70 @@
 package com.example.a48900.ledsistem;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
-import android.os.AsyncTask;
-import android.util.Log;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 public class MainActivity extends AppCompatActivity {
+
+    EditText txt_username, txt_password;
     Button button_login;
-
-
+    String url = "http://140.213.15.233/led/api/automation/search/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_username = (EditText) findViewById(R.id.txt_username);
+        txt_password = (EditText) findViewById(R.id.txt_password);
+        button_login = (Button) findViewById(R.id.button_login);
 
-        //method pindah layout
-        button_login= (Button) findViewById(R.id.button_login);
+        Button button_login = (Button) findViewById(R.id.button_login);
+
         button_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent i= new Intent(getApplicationContext(),Main2Activity.class);
-                startActivity(i);
+            public void onClick(View v) {
+                JSONObject postData = new JSONObject();
+                try {
+                    postData.put("userName", txt_username.getText().toString());
+                    postData.put("password", txt_password.getText().toString());
 
+
+                    new SendDeviceDetails.execute("http://140.213.15.233/led/api/automation/search/", postData.toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
-
-
         });
-    }
 
+    }
 }
 
